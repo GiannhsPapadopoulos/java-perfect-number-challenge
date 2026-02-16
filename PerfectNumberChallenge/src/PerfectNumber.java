74b518ad0494526b2d42788be0d1b9ecb0d78937
JP <papadopoulos.giannhs.soft@gmail.com>
2026-02-16 19:54:36 +0200

    Add files via upload

---
+++ PerfectNumberChallenge/src/PerfectNumber.java
@@ -0,0 +1,20 @@
+public class PerfectNumber {
+    // write code here
+    public static boolean isPerfectNumber(int number) {
+
+        if (number < 1) {
+            return false;
+        }
+
+        int i = 1;
+        int sum = 0;
+        while (i < number) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+            i++;
+        }
+
+        return (sum == number);
+    }
+}
